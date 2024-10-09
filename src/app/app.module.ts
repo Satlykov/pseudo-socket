0ef5c8a310e70886e49fff4083347230c232489c
Rustam Satlykov <rustam.satlykov@proton.me>
2024-10-10 01:58:15 +0500

    feat: add main-table component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildTableComponent } from './components/child-table/child-table.component';
+import { MainTableComponent } from './components/main-table/main-table.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ChildTableComponent,
+		MainTableComponent,
+		HeaderComponent,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
